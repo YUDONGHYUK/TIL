81c36c3e28ad17e18ccf63a6ab78ba3a7d52ecce
doyu <ydh0905@nate.com>
2022-06-21 15:16:49 +0900

    prime number

---
+++ codevolution/algorithm/prime-number/prime-number.js
@@ -0,0 +1,38 @@
+/*
+Problem
+
+Give a natural number ‘n’, determine if the number is prime or not
+
+isPrime(5) - true(1*5 or 5*1)
+isPrime(4) - false(1*4 or 2*2 or 4*1)
+*/
+
+// function isPrime(n) {
+//   if (n < 2) {
+//     return false;
+//   }
+//   for (let i = 2; i < n; i++) {
+//     if (n % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(1)); // false
+console.log(isPrime(5)); // true
+console.log(isPrime(4)); // false
+
+// Big-O = O(sqrt(n))
