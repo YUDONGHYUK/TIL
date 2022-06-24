6c45d992617627df21598d4e2ed68297be70673e
doyu <ydh0905@nate.com>
2022-06-24 17:22:23 +0900

    react-hook: useState & useEffect

--- codevolution/react-hooks/src/App.js
+++ codevolution/react-hooks/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import IntervalClassCounter from './components/useEffect/IntervalClassCounter';
+import IntervalHookCounter from './components/useEffect/IntervalHookCounter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <IntervalClassCounter />
+      <IntervalHookCounter />
     </div>
   );
 }
