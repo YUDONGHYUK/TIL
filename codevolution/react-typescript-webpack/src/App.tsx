5f93a1eff04a19e350921795bc90230b61dac97e
doyu <ydh0905@nate.com>
2022-06-27 18:45:05 +0900

    react-typescript-webpack: configuring webpack in different envirionment(dev, prod)

--- codevolution/react-typescript-webpack/src/App.tsx
+++ codevolution/react-typescript-webpack/src/App.tsx
@@ -5,7 +5,10 @@ import LOGO from './logo.svg';
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV},{' '}
+        {process.env.name}
+      </h1>
       <img src={IMAGE} alt='React logo' width='300' height='250' />
       <img src={LOGO} alt='React logo' width='300' />
     </>
