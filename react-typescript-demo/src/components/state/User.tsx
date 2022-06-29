2dcdbe4f313f14a20e91d62dc6ddf1392d1f7be9
doyu <ydh0905@nate.com>
2022-06-29 19:03:49 +0900

    react-typescript-demo: type assertion

--- react-typescript-demo/src/components/state/User.tsx
+++ react-typescript-demo/src/components/state/User.tsx
@@ -6,23 +6,26 @@ type AuthUser = {
 };
 
 export const User = () => {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  // const [user, setUser] = useState<AuthUser | null>(null);
+  const [user, setUser] = useState<AuthUser>({} as AuthUser); // type assertion
   const handleLogin = () => {
     setUser({
       name: 'doyu',
       email: 'doyu@example.com',
     });
   };
-  const handleLogout = () => {
-    setUser(null);
-  };
+  // const handleLogout = () => {
+  //   setUser(null);
+  // };
 
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>User name is {user?.name}</div>
-      <div>User email is {user?.email}</div>
+      {/* <button onClick={handleLogout}>Logout</button> */}
+      {/* <div>User name is {user?.name}</div> */}
+      {/* <div>User email is {user?.email}</div> */}
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
     </div>
   );
 };
