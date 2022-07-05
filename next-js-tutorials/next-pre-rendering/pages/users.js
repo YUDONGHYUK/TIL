debb0229a3a8e1ccb2527c4a71b6ccfa87c56ab1
doyu <ydh0905@nate.com>
2022-07-05 16:53:48 +0900

    next-js-tutorials: Static generation with getStaticProps

---
+++ next-js-tutorials/next-pre-rendering/pages/users.js
@@ -0,0 +1,26 @@
+function UserList({ users }) {
+  return (
+    <>
+      <h1>List of users</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default UserList;
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  return {
+    props: { users: data },
+  };
+}
