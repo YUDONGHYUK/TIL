017dcee43264716039d018d709719ae0c09b00f7
doyu <ydh0905@nate.com>
2022-07-11 21:49:57 +0900

    react-storybook: Addons

--- react-storybook/src/components/ChakraButton/ChakraButton.stories.js
+++ react-storybook/src/components/ChakraButton/ChakraButton.stories.js
@@ -4,7 +4,25 @@ import { Button } from '@chakra-ui/react';
 export default {
   title: 'Chakra/Button',
   component: Button,
+  argTypes: { onClick: { action: 'clicked' } },
 };
 
-export const Success = () => <Button colorScheme='green'>Success</Button>;
-export const Danger = () => <Button colorScheme='red'>Danger</Button>;
+// args 메커니즘 사용
+const Template = (args) => <Button {...args} />;
+
+export const Success = Template.bind({});
+Success.args = {
+  colorScheme: 'green',
+  children: 'Success',
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  colorScheme: 'red',
+  children: 'Danger',
+  onClick: () => console.log('click?'),
+};
+
+// 기존 코드
+// export const Success = () => <Button colorScheme='green'>Success</Button>;
+// export const Danger = () => <Button colorScheme='red'>Danger</Button>;
