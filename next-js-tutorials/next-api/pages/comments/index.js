51d5486de7dcfc17bb6f95c7f8cd5f6d45380dc4
doyu <ydh0905@nate.com>
2022-07-12 14:47:00 +0900

    next-js-tutorials: API POST request

--- next-js-tutorials/next-api/pages/comments/index.js
+++ next-js-tutorials/next-api/pages/comments/index.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 function CommentsPage() {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState('');
 
   const fetchCommets = async () => {
     const response = await fetch('/api/comments');
@@ -9,8 +10,26 @@ function CommentsPage() {
     setComments(data);
   };
 
+  const submitComment = async () => {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({ comment }),
+      headers: {
+        'Content-Type': 'application/json', // json 파일을 전송하기 때문에 설정.
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <>
+      <input
+        type='text'
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
+      <button onClick={submitComment}>Submit comment</button>
       <button onClick={fetchCommets}>Load Comments</button>
       {comments.map((comment) => {
         return (
