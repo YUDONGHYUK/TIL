8077ec4b33c22e7fea50d7523499f8d97ff8f393
doyu <ydh0905@nate.com>
2022-07-12 16:16:35 +0900

    next-js-tutorials: API DELETE request

--- next-js-tutorials/next-api/pages/comments/index.js
+++ next-js-tutorials/next-api/pages/comments/index.js
@@ -22,6 +22,15 @@ function CommentsPage() {
     console.log(data);
   };
 
+  const deleteComment = async (commentId) => {
+    const response = await fetch(`/api/comments/${commentId}`, {
+      method: 'DELETE',
+    });
+    const data = await response.json();
+    console.log(data);
+    fetchCommets();
+  };
+
   return (
     <>
       <input
@@ -35,6 +44,7 @@ function CommentsPage() {
         return (
           <div key={comment.id}>
             {comments.id} {comment.text}
+            <button onClick={() => deleteComment(comment.id)}>Delete</button>
           </div>
         );
       })}
