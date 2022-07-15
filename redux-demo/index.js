f41b55bcd67eaca9bf702f428f6ba67b4f06044f
doyu <ydh0905@nate.com>
2022-07-16 00:59:03 +0900

    redux-demo: actions

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,9 @@
-console.log('From index.js');
+const BUY_CAKE = 'BUY_CAKE';
+
+// action creator -> return the action
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action',
+  };
+}
