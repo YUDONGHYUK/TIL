f4ffaea3ad766a6d2b1d39434d1616216b95dd46
doyu <ydh0905@nate.com>
2022-07-15 17:59:36 +0900

    next-js-tutorials: securing pages client-side

--- next-js-tutorials/next-auth/components/Navbar.js
+++ next-js-tutorials/next-auth/components/Navbar.js
@@ -1,13 +1,15 @@
 import Link from 'next/link';
-import { signIn, signOut } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 function Navbar() {
+  const { data: session, status } = useSession();
+  console.log(useSession());
   return (
     <nav className='header'>
       <h1 className='logo'>
         <a href='#'>NextAuth</a>
       </h1>
-      <ul className={`main-nav`}>
+      <ul className={`main-nav ${status === 'loading' ? 'loading' : 'loaded'}`}>
         <li>
           <Link href='/'>
             <a>Home</a>
@@ -24,30 +26,34 @@ function Navbar() {
           </Link>
         </li>
 
-        <li>
-          <Link href='/api/auth/signin'>
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                signIn('github');
-              }}
-            >
-              Sign In
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/api/auth/signout'>
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                signOut();
-              }}
-            >
-              Sign Out
-            </a>
-          </Link>
-        </li>
+        {!session && status === 'unauthenticated' && (
+          <li>
+            <Link href='/api/auth/signin'>
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn('github');
+                }}
+              >
+                Sign In
+              </a>
+            </Link>
+          </li>
+        )}
+        {session && status === 'authenticated' && (
+          <li>
+            <Link href='/api/auth/signout'>
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  signOut();
+                }}
+              >
+                Sign Out
+              </a>
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
--- next-js-tutorials/next-auth/pages/dashboard.js
+++ next-js-tutorials/next-auth/pages/dashboard.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+
 function Dashboard() {
+  const { status } = useSession();
+
+  useEffect(() => {
+    if (status === 'unauthenticated') signIn();
+  }, [status]);
+
+  if (status !== 'authenticated') return <h1>Loading...</h1>;
   return <h1>Dashboard Page</h1>;
 }
 
