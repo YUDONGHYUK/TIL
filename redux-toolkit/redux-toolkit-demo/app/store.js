4a211ab887578cdf49d03e4f60dbb5e4e25d5752
doyu <ydh0905@nate.com>
2022-07-21 12:37:26 +0900

    redux-toolkit-demo: extraReducers

--- redux-toolkit/redux-toolkit-demo/app/store.js
+++ redux-toolkit/redux-toolkit-demo/app/store.js
@@ -1,16 +1,18 @@
 const configureStore = require('@reduxjs/toolkit').configureStore;
-const reduxLogger = require('redux-logger');
+// const reduxLogger = require('redux-logger');
 const cakeReducer = require('../features/cake/cakeSlice');
 const icecreamReducer = require('../features/icecream/icecreamSlice');
 
-const logger = reduxLogger.createLogger();
+// const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
+
+// extraReducers를 간단히 살펴보기 위해 logger 미들웨어 주석처리
