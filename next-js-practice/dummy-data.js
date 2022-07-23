ba5c644a0e137015510d761fbbcca3b813ea5a17
doyu <ydh0905@nate.com>
2022-07-23 19:48:35 +0900

    next-js-practice: create AllEventPage component

--- next-js-practice/dummy-data.js
+++ next-js-practice/dummy-data.js
@@ -35,6 +35,10 @@ export function getFeaturedEvents() {
   return DUMMY_EVENTS.filter((event) => event.isFeatured);
 }
 
+export function getAllEvents() {
+  return DUMMY_EVENTS;
+}
+
 export function getEventById(id) {
   return DUMMY_EVENTS.find((event) => event.id === id);
 }
