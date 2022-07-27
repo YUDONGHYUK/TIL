2965018bf2ba776cbf117d550357faa5b59e86e8
doyu <ydh0905@nate.com>
2022-07-27 23:55:04 +0900

    next-js-blog: change from dummy data to markdown data

--- next-js-blog/components/posts/post-detail/post-content.js
+++ next-js-blog/components/posts/post-detail/post-content.js
@@ -2,22 +2,14 @@ import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header';
 import styles from './post-content.module.css';
 
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextjs',
-  title: 'Getting Started with NextJS',
-  image: 'getting-started-nextjs.png',
-  date: '2022-02-10',
-  content: '# This is a first post',
-};
-
-function PostContent() {
+function PostContent({ post }) {
   return (
     <article className={styles.content}>
       <PostHeader
-        title={DUMMY_POST.title}
-        image={`/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`}
+        title={post.title}
+        image={`/images/posts/${post.slug}/${post.image}`}
       />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
