d58e71325217c43740e6b1fecf1e1eee33ecd797
doyu <ydh0905@nate.com>
2022-07-27 15:29:52 +0900

    next-js-blog: create Hero component

---
+++ next-js-blog/components/home-page/hero.js
@@ -0,0 +1,23 @@
+import Image from 'next/future/image';
+import styles from './hero.module.css';
+
+function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.image}>
+        <Image
+          src='/images/site/doyu.jpg'
+          alt='An image showing Doyu'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Doyu</h1>
+      <p>
+        I blog about web development - especially fronted frameworks like React
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
