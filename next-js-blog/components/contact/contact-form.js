ec6bda7ef67f91e0df6786af89b2bace5a0701ff
doyu <ydh0905@nate.com>
2022-07-29 18:08:40 +0900

    next-js-blog: storing message with mongodb in database

--- next-js-blog/components/contact/contact-form.js
+++ next-js-blog/components/contact/contact-form.js
@@ -1,23 +1,42 @@
+import { useRef } from 'react';
 import styles from './contact-form.module.css';
 
 function ContactForm() {
+  const emailInputRef = useRef();
+  const nameInputRef = useRef();
+  const messageInputRef = useRef();
+
+  function sendMessageHandler(e) {
+    e.preventDefault();
+
+    fetch('/api/contact', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: emailInputRef.current.value,
+        name: nameInputRef.current.value,
+        message: messageInputRef.current.value,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
   return (
     <section className={styles.contact}>
       <h1>How can I help you?</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={sendMessageHandler}>
         <div className={styles.controls}>
           <div className={styles.control}>
             <label htmlFor='email'>Your Email</label>
-            <input type='email' id='email' required />
+            <input type='email' id='email' ref={emailInputRef} required />
           </div>
           <div className={styles.control}>
             <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name' required />
+            <input type='text' id='name' ref={nameInputRef} required />
           </div>
         </div>
         <div className={styles.control}>
           <label htmlFor='message'>Your Message</label>
-          <textarea id='message' rows='5' />
+          <textarea id='message' rows='5' ref={messageInputRef} required />
         </div>
         <div className={styles.actions}>
           <button>Send Message</button>
