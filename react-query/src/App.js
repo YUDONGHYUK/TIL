6a826ac28557a401f084d43840e94a9a143e2a46
doyu <ydh0905@nate.com>
2022-08-12 11:50:41 +0900

    react-query: basic fetching data with useQuery

--- react-query/src/App.js
+++ react-query/src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import HomePage from './components/Home.page';
 import RQSuperHeroesPage from './components/RQSuperHeroes.page';
 import SuperHeroesPage from './components/SuperHeroes.page';
 import './App.css';
+
+const queryClient = new QueryClient();
 
 function App() {
   const NavLinkStyles = ({ isActive }) => {
@@ -13,34 +16,36 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink style={NavLinkStyles} to='/'>
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink style={NavLinkStyles} to='/super-heroes'>
-                Traditional Super Heroes
-              </NavLink>
-            </li>
-            <li>
-              <NavLink style={NavLinkStyles} to='/rq-super-heroes'>
-                RQ Super Heroes
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='/super-heroes' element={<SuperHeroesPage />} />
-          <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink style={NavLinkStyles} to='/'>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink style={NavLinkStyles} to='/super-heroes'>
+                  Traditional Super Heroes
+                </NavLink>
+              </li>
+              <li>
+                <NavLink style={NavLinkStyles} to='/rq-super-heroes'>
+                  RQ Super Heroes
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/super-heroes' element={<SuperHeroesPage />} />
+            <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- react-query/src/components/RQSuperHeroes.page.js
+++ react-query/src/components/RQSuperHeroes.page.js
@@ -1,7 +1,27 @@
 // fetch data using react-query
 
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes');
+};
+
 const RQSuperHeroesPage = () => {
-  return <div>Rq Super Heroes Page</div>;
+  const { isLoading, data } = useQuery(['super-heroes'], fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Rq Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>;
+      })}
+    </>
+  );
 };
 
 export default RQSuperHeroesPage;
