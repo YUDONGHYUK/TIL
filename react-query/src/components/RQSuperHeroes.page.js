54bd6d1688c99b915bc3a4e8995d30bcb2c63115
doyu <ydh0905@nate.com>
2022-08-16 01:13:05 +0900

    react-query: success and error callback

--- react-query/src/components/RQSuperHeroes.page.js
+++ react-query/src/components/RQSuperHeroes.page.js
@@ -4,10 +4,18 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 const fetchSuperHeroes = () => {
-  return axios.get('http://localhost:4000/superheroes');
+  return axios.get('http://localhost:4000/superheroes1');
 };
 
 const RQSuperHeroesPage = () => {
+  const onSuccess = (data) => {
+    console.log('Perform side effect after data fetching', data);
+  };
+
+  const onError = (error) => {
+    console.log('Perform side effect after encountering error', error);
+  };
+
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     ['super-heroes'],
     fetchSuperHeroes,
@@ -17,11 +25,13 @@ const RQSuperHeroesPage = () => {
       // refetchOnWindowFocus: true,
       // refetchInterval: 2000,
       // refetchIntervalInBackground: true,
-      enabled: false,
+      // enabled: false,
+      onSuccess,
+      onError,
     }
   );
 
-  console.log({ isLoading, isFetching });
+  console.log({ isLoading, isFetching, isError });
 
   if (isFetching) {
     return <h2>Loading...</h2>;
