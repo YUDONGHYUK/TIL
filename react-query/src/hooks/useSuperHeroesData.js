71e92e280fa4fca16ca5b063f967592e6f56e263
doyu <ydh0905@nate.com>
2022-08-24 17:44:15 +0900

    react-query: query invalidation

--- react-query/src/hooks/useSuperHeroesData.js
+++ react-query/src/hooks/useSuperHeroesData.js
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
 const fetchSuperHeroes = () => {
@@ -22,5 +22,11 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 };
 
 export const useAddSuperHeroData = () => {
-  return useMutation(addSuperHero);
+  const queryClient = useQueryClient();
+
+  return useMutation(addSuperHero, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['super-heroes']);
+    },
+  });
 };
