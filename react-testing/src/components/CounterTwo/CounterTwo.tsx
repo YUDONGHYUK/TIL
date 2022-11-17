3d1640adbecedc8aadc4c40aff5ae8e291e88388
doyu <ydh0905@nate.com>
2022-11-17 17:18:17 +0900

    react-testing: mocking function

---
+++ react-testing/src/components/CounterTwo/CounterTwo.tsx
@@ -0,0 +1,18 @@
+import { CounterTowProps } from './CounterTwo.types';
+
+const CounterTwo = ({
+  count,
+  handleIncrement,
+  handleDecrement,
+}: CounterTowProps) => {
+  return (
+    <div>
+      <h1>Counter Two</h1>
+      <p>{count}</p>
+      {handleIncrement && <button onClick={handleIncrement}>Increment</button>}
+      {handleDecrement && <button onClick={handleDecrement}>Decrement</button>}
+    </div>
+  );
+};
+
+export default CounterTwo;
---
+++ react-testing/src/components/CounterTwo/CounterTwo.types.ts
@@ -0,0 +1,5 @@
+export type CounterTowProps = {
+  count: number;
+  handleIncrement?: () => {};
+  handleDecrement?: () => {};
+};
